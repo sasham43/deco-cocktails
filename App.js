603c7dc9e6fa06eed3c045be0944ddb20eeb04b6
Sasha Kramer <sashasemail@gmail.com>
2020-10-15 17:01:58 -0400

    add main

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+// modules
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// components
+import Main from './components/Main'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Crump Cocktails</Text>
       <StatusBar style="auto" />
+      <Main></Main>
     </View>
   );
 }
---
+++ components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class Main extends React.component {
+
+    render() {
+        return (
+            <View>
+                <Text>Main component</Text>
+            </View>
+        )
+    }
+}
