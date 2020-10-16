72bfbe46d499f97270ce1b5306838b16fa7c0e38
Sasha Kramer <sashasemail@gmail.com>
2020-10-15 20:24:16 -0400

    add routing

---
+++ components/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import AppText from './AppText'
+
+
+export default function About (){
+    return (
+        <View style={styles.about}>
+            <AppText>This app was made in Crump House with an assist from Bongo and Gomez.</AppText>
+        </View>
+    )
+}
+// export default class About extends React.Component {
+//     render(){
+//         return (
+//             <View style={styles.about}>
+//                 <AppText>This app was made in Crump House with an assist from Bongo and Gomez.</AppText>
+//             </View>
+//         )
+//     }
+// }
+
+const styles = StyleSheet.create({
+    about: {
+        flex: 1,
+        alignSelf: 'center'
+    }
+})
--- components/Main.js
+++ components/Main.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { NativeRouter, Route, Link } from "react-router-native"
 
 import Title from './Title'
 import CocktailList from './CocktailList'
+import AppText from './AppText'
+import About from './About'
 
 export default class Main extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Title></Title>
-                {/* <Text>Main component</Text> */}
-                <CocktailList></CocktailList>
-            </View>
+                <NativeRouter>
+                    <View style={styles.container}>
+                        <Title></Title>
+                        <View>
+                            <Link to="/about">
+                                <AppText>About</AppText>
+                            </Link>
+                            <Link to="/">
+                                <AppText>Cocktails</AppText>
+                            </Link>
+                        </View>
+
+                        <Route exact path="/">
+                            <CocktailList />
+                        </Route>
+                        <Route exact path="/about">
+                            <About />
+                        </Route>
+                        {/* <Route exact path="/" component={CocktailList} />
+                        <Route path="/about" component={About} /> */}
+                    </View>
+                </NativeRouter>
         )
     }
 }
@@ -21,6 +41,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
-        marginTop: 50
+        alignSelf: 'stretch',
+        marginTop: 50,
+        marginLeft: 50,
+        marginRight: 50
     }
 })
