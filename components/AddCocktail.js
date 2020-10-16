963ae197ec32051aebd01512548bf7b66bf397d9
Sasha Kramer <sashasemail@gmail.com>
2020-10-16 17:28:34 -0400

    adding list of ingredients

--- components/AddCocktail.js
+++ components/AddCocktail.js
@@ -1,21 +1,60 @@
 import React, { useState } from 'react'
 import { View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import { generate } from 'shortid'
+// import { Picker } from '@react-native-community/picker'
+import RNPickerSelect from 'react-native-picker-select'
 
 import AppText from './AppText'
 import { useCocktails } from '../utils/hooks'
 
 export default function Add(){
     const [ newCocktailName, setNewCocktailName ] = useState('')
+    const [ newCocktailIngredients, setNewCocktailIngredients ] = useState([{
+        id: generate(),
+        ingredient_name: '',
+        parts: 0
+    }])
+
     const { cocktails, addCocktail } = useCocktails([])
 
-    function NewIngredient(){
+    function NewIngredient(props) {
+        const placeholder = {
+            label: 'Parts...',
+            value: null,
+            color: '#9EA0A4',
+        };
         return (
-            <View>
-                <TextInput style={styles.input} placeholder="Parts..." keyboardType="numeric" />
+            <View key={props.id}>
+                <RNPickerSelect
+                    placeholder={placeholder}
+                    useNativeAndroidPickerStyle={false}
+                style={styles} onValueChange={(val)=>console.log(val)} items={[
+                    {
+                        label: '1',
+                        value: 1
+                    },
+                    {
+                        label: '2',
+                        value: 2
+                    },
+                    {
+                        label: '3',
+                        value: 3
+                    },
+                ]} />
+
+                {/* </RNPickerSelect> */}
                 <TextInput style={styles.input} placeholder="Ingredient..." />
             </View>
         )
+    }
+
+    function NewIngredientMap(){
+        return newCocktailIngredients.map(ingredient=>{
+            return (
+                <NewIngredient id={ingredient.id} ingredient_name={ingredient.name} parts={ingredient.parts} />
+            )
+        })
     }
 
     return (
@@ -26,9 +65,16 @@ export default function Add(){
                     value={newCocktailName}
                     onChangeText={text => setNewCocktailName(text)}
                     style={styles.input}
-                    placeholder="Cocktail name..."
+                    placeholder="New cocktail name..."
                 />
-                <NewIngredient />
+                <NewIngredientMap />
+                <TouchableOpacity onPress={()=>setNewCocktailIngredients([...newCocktailIngredients, {
+                    id: generate(),
+                    ingredient_name: '',
+                    parts: 0
+                }])}>
+                    <AppText style={styles.add_ingredient_button}>+</AppText>
+                </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => {
                     addCocktail({
@@ -60,5 +106,19 @@ const styles = StyleSheet.create({
         fontSize: 23,
         alignSelf: 'center',
         // justifySelf: 'center'
+    },
+    add_ingredient_button: {
+        marginTop: 15,
+        fontSize: 23,
+        alignSelf: 'center',
+        // justifySelf: 'center'
+    },
+    inputIOS: {
+        fontFamily: 'PoiretOne_400Regular',
+        borderColor: '#eee',
+        borderWidth: 1,
+        // borderRadius: 5,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
     }
 })
