9a39376c85688d44544763055c911739c9a87a15
Sasha Kramer <sashasemail@gmail.com>
2020-10-18 16:51:57 -0400

    get form on same line

--- components/AddStock.js
+++ components/AddStock.js
@@ -5,6 +5,7 @@ import AppText from './AppText'
 import { useStock } from '../utils/hooks'
 
 const windowHeight = Dimensions.get('window').height
+const windowWidth = Dimensions.get('window').width
 
 export default function AddStock(){
 
@@ -12,17 +13,19 @@ export default function AddStock(){
   
     return (
         <View style={styles.view}>
-            <View>
-                <Switch value={newStockIn} trackColor={{ false: 'grey', true: 'black' }} onValueChange={(val) => setNewStockIn(val)} />
+            <View style={styles.stock_form}>
+                <View style={styles.switch_container}>
+                    <Switch value={newStockIn} trackColor={{ false: 'grey', true: 'black' }} onValueChange={(val) => setNewStockIn(val)} />
+                </View>
+                <View style={styles.input_container}>
+                    <TextInput style={styles.input} value={newStockName} onChangeText={text => setNewStockName(text)} placeholder="New stock..." />
+                </View>
             </View>
-            <View>
-                <TextInput style={styles.input} value={newStockName} onChangeText={ text => setNewStockName(text)} placeholder="New stock..." />
-            </View>
-            <View>
+            <View style={styles.add_container}>
                 <TouchableOpacity onPress={async () => {
                     addToStock()
                 }}>
-                    <AppText style={styles.add_button}>Add Bottle To Stock</AppText>
+                    <AppText style={styles.link_text}>Add Bottle To Stock</AppText>
                 </TouchableOpacity>
             </View>
         </View>
@@ -35,8 +38,16 @@ const styles = StyleSheet.create({
         paddingTop: 10,
         paddingLeft: 10,
         paddingRight: 10,
-        height: windowHeight - 100
+        height: windowHeight - 100,
+        // flexDirection: 'row'
     },
+    stock_form: {
+        flexDirection: 'row',
+        // flexDirection: 'column',
+        alignContent: 'center',
+        // justifyContent: 'flex-end'
+        // flex: 1
+    },  
     text: {
         // fontSize: 50,
         // color: 'red',
@@ -50,6 +61,27 @@ const styles = StyleSheet.create({
         paddingRight: 10,
         borderWidth: 1,
         borderColor: '#eee',
-        borderStyle: 'solid'
+        borderStyle: 'solid',
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        // width: windowWidth - 150
     },
+    add_container: {
+        marginTop: 50
+    },  
+    link_text: {
+        fontSize: 22,
+        // color: 'red',
+        textAlign: 'center'
+    },
+    switch_container: {
+        alignSelf: 'center'
+    },
+    input_container: {
+        alignSelf: 'center',
+        flex: 1,
+        marginLeft: 10
+        // flexDirection: 'row',
+        // flex: 1
+    }
 })
