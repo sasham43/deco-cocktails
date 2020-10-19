533d7cd6bb134750b2cca55375b5987a9a9bad5e
Sasha Kramer <sashasemail@gmail.com>
2020-10-19 17:16:48 -0400

    move parts and shapes to separate components

--- components/CocktailList.js
+++ components/CocktailList.js
@@ -4,10 +4,11 @@ import { generate } from 'shortid'
 import _ from 'lodash'
 
 import AppText from './AppText'
-import HalfCircle from '../assets/half-circle.svg'
-import QuarterCircle from '../assets/quarter-circle.svg'
-import ThreeQuarterCircle from '../assets/three-quarter-circle.svg'
-import Circle from '../assets/circle.svg'
+import { Part, PartMap } from './Parts'
+// import HalfCircle from '../assets/half-circle.svg'
+// import QuarterCircle from '../assets/quarter-circle.svg'
+// import ThreeQuarterCircle from '../assets/three-quarter-circle.svg'
+// import Circle from '../assets/circle.svg'
 
 
 import { useCocktails, useStock } from '../utils/hooks'
@@ -21,98 +22,98 @@ function ClassListMap() {
         const { cocktails } = useCocktails()
         const { isInStock } = useStock()
 
-        function buildPartArray(parts){
-            var part_array = []
+        // function buildPartArray(parts){
+        //     var part_array = []
 
-            var remainder = parts.toString().split('.')[1]
+        //     var remainder = parts.toString().split('.')[1]
 
-            if(parts >= 1){
-                for (var i = 1; i <= parts; i++){
-                    part_array.push(1)
-                }
-            }
-            if(remainder != undefined){
-                remainder = Number("." + remainder)
-                part_array.push(remainder)
-            }
+        //     if(parts >= 1){
+        //         for (var i = 1; i <= parts; i++){
+        //             part_array.push(1)
+        //         }
+        //     }
+        //     if(remainder != undefined){
+        //         remainder = Number("." + remainder)
+        //         part_array.push(remainder)
+        //     }
 
-            // console.log('part array', part_array, parts)
+        //     // console.log('part array', part_array, parts)
 
-            return part_array
-        }
+        //     return part_array
+        // }
 
-        function Shape(props){
-            // console.log("Shape props", props)
-            if (props.part == 0.25) {
-                return (
-                    <QuarterCircle width={props.width} height={props.height} />
-                )
-            }
-            if (props.part == 0.5) {
-                return (
-                    <HalfCircle width={props.width} height={props.height} />
-                )
-            }
-            if (props.part == 0.75) {
-                return (
-                    <ThreeQuarterCircle width={props.width} height={props.height} />
-                )
-            }
-            if (props.part == 1) {
-                return (
-                    <Circle width={props.width} height={props.height} />
-                )
-            }
-            return null
-        }
-        function ShapeMap(props){
-            var shape_array = buildPartArray(props.parts)
-            return shape_array.map((part, i) => {
-                var key = generate()
-                return (
-                    <View key={key} style={[styles.shape_container, getShapeMargin(part)]}>
-                        <Shape height={9} width={9} part={part} />
-                    </View>
-                )
-            })
-        }
-        function getShapeMargin(part){
-            // console.log('width for part', part)
-            if(part == 0.25 || part == 0.25){
-                return {
-                    marginLeft: -10
-                }
-            }
-            return 25
-        }
+        // function Shape(props){
+        //     // console.log("Shape props", props)
+        //     if (props.part == 0.25) {
+        //         return (
+        //             <QuarterCircle width={props.width} height={props.height} />
+        //         )
+        //     }
+        //     if (props.part == 0.5) {
+        //         return (
+        //             <HalfCircle width={props.width} height={props.height} />
+        //         )
+        //     }
+        //     if (props.part == 0.75) {
+        //         return (
+        //             <ThreeQuarterCircle width={props.width} height={props.height} />
+        //         )
+        //     }
+        //     if (props.part == 1) {
+        //         return (
+        //             <Circle width={props.width} height={props.height} />
+        //         )
+        //     }
+        //     return null
+        // }
+        // function ShapeMap(props){
+        //     var shape_array = buildPartArray(props.parts)
+        //     return shape_array.map((part, i) => {
+        //         var key = generate()
+        //         return (
+        //             <View key={key} style={[styles.shape_container, getShapeMargin(part)]}>
+        //                 <Shape height={9} width={9} part={part} />
+        //             </View>
+        //         )
+        //     })
+        // }
+        // function getShapeMargin(part){
+        //     // console.log('width for part', part)
+        //     if(part == 0.25 || part == 0.25){
+        //         return {
+        //             marginLeft: -10
+        //         }
+        //     }
+        //     return 25
+        // }
 
-        function Part(props){
-            if(props.last){
-                return (
-                    <View style={{flexDirection:'row'}}>
-                        <ShapeMap parts={props.parts} last={props.last} />
-                    </View>
-                )
-            } else {
-                return (
-                    <View style={{ flexDirection: 'row', marginRight: 10 }}>
-                        <ShapeMap parts={props.parts} last={props.last} />
-                        <AppText>|</AppText>
-                    </View>
-                )
-            }
-        }
-        function PartMap(props){
-            return (
-                <View style={styles.part_map_container}>
-                    {props.ingredients.map((ingredient, i) => (
-                        <View style={styles.part_container} key={`part-${i}`}>
-                            <Part parts={ingredient.parts} last={(i + 1 == props.ingredients.length)} />
-                        </View>
-                    ))}
-                </View>
-            )
-        } 
+        // function Part(props){
+        //     if(props.last){
+        //         return (
+        //             <View style={{flexDirection:'row'}}>
+        //                 <ShapeMap parts={props.parts} last={props.last} />
+        //             </View>
+        //         )
+        //     } else {
+        //         return (
+        //             <View style={{ flexDirection: 'row', marginRight: 10 }}>
+        //                 <ShapeMap parts={props.parts} last={props.last} />
+        //                 <AppText>|</AppText>
+        //             </View>
+        //         )
+        //     }
+        // }
+        // function PartMap(props){
+        //     return (
+        //         <View style={styles.part_map_container}>
+        //             {props.ingredients.map((ingredient, i) => (
+        //                 <View style={styles.part_container} key={`part-${i}`}>
+        //                     <Part parts={ingredient.parts} last={(i + 1 == props.ingredients.length)} />
+        //                 </View>
+        //             ))}
+        //         </View>
+        //     )
+        // } 
         function Name(props){
             if(props.last){
                 return (
---
+++ components/Shapes.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { generate } from 'shortid'
+
+import HalfCircle from '../assets/half-circle.svg'
+import QuarterCircle from '../assets/quarter-circle.svg'
+import ThreeQuarterCircle from '../assets/three-quarter-circle.svg'
+import Circle from '../assets/circle.svg'
+
+import AppText from './AppText'
+
+function buildPartArray(parts) {
+    var part_array = []
+
+    var remainder = parts.toString().split('.')[1]
+
+    if (parts >= 1) {
+        for (var i = 1; i <= parts; i++) {
+            part_array.push(1)
+        }
+    }
+    if (remainder != undefined) {
+        remainder = Number("." + remainder)
+        part_array.push(remainder)
+    }
+
+    // console.log('part array', part_array, parts)
+
+    return part_array
+}
+
+export function Shape(props) {
+    // console.log("Shape props", props)
+    if (props.part == 0.25) {
+        return (
+            <QuarterCircle width={props.width} height={props.height} />
+        )
+    }
+    if (props.part == 0.5) {
+        return (
+            <HalfCircle width={props.width} height={props.height} />
+        )
+    }
+    if (props.part == 0.75) {
+        return (
+            <ThreeQuarterCircle width={props.width} height={props.height} />
+        )
+    }
+    if (props.part == 1) {
+        return (
+            <Circle width={props.width} height={props.height} />
+        )
+    }
+    return null
+}
+export function ShapeMap(props) {
+    var shape_array = buildPartArray(props.parts)
+    return shape_array.map((part, i) => {
+        var key = generate()
+        return (
+            <View key={key} style={[styles.shape_container, getShapeMargin(part)]}>
+                <Shape height={9} width={9} part={part} />
+            </View>
+        )
+    })
+}
+function getShapeMargin(part) {
+    // console.log('width for part', part)
+    if (part == 0.25 || part == 0.25) {
+        return {
+            marginLeft: -10
+        }
+    }
+    return 25
+}
+
+
+
+const styles = StyleSheet.create({
+    shape_container: {
+        flexDirection: 'row',
+        marginRight: 10,
+        alignItems: 'center'
+        // width: 100
+        // flex: 3
+    }
+})
