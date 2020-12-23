a73cbec93c75fa4ad04fa1102e0c8aef44475bc9
Sasha Kramer <sashasemail@gmail.com>
2020-12-23 13:30:19 -0500

    add header icon

---
+++ components/HeaderIcon.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Animated, View } from 'react-native'
+
+// import AppText from './AppText'
+import InStockIcon from '../assets/in-stock'
+
+export default function HeaderIcon(props){
+    var rotate = props.direction == 'left' ? '135deg' : '-45deg'
+    var padding = props.direction == 'left' ? {paddingLeft: 10} : {paddingRight: 10}
+    return (
+        <Animated.View style={[padding, { opacity: props.anim }]}>
+            <InStockIcon transform={[{ rotate: rotate }]} width={30} height={30} fill={props.ui.current_theme.color} />
+        </Animated.View>
+    )
+}
+
+{/* <InStockIcon transform={[{ rotate: '135deg' }]} width={30} height={30} fill={props.ui.current_theme.color} /> */}
