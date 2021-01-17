7ccb03544b95f3c3569b4c4aab0377476bafa591
Sasha Kramer <sashasemail@gmail.com>
2021-01-17 10:27:16 -0500

    stylize select icons

--- components/Menu.js
+++ components/Menu.js
@@ -5,8 +5,9 @@ import Carousel from 'react-native-snap-carousel'
 
 import AppText from './AppText'
 import InStockIcon from '../assets/in-stock'
-import MenuSelectIcon from '../assets/menu-select'
-import HeaderIcon from './HeaderIcon'
+// import MenuSelectIcon from '../assets/menu-select'
+import CornerIcon from '../assets/corner'
+// import HeaderIcon from './HeaderIcon'
 
 const windowWidth = Dimensions.get('window').width
 
@@ -103,17 +104,20 @@ function Menu(props) {
 
     // const carouse
     const [carousel, setCarousel] = useState(null)
-    const icon_size = 90
+    // const icon_size = 90
 
     function renderMenuItem(props){
 
         return (
+            // <AppButton press={()=>pressItem(props)}>
+            //     {props.item.name}
+            // </AppButton>
             <Pressable onPress={()=>pressItem(props)} style={{justifyContent: 'center', alignContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
                 <AppText style={{fontSize: 20}}>{props.item.name}</AppText>
             </Pressable>
         )
     }
-
+    const icon_size = 15
     return (
         <View style={[styles.menu, {borderColor: '#000', borderWidth:0}, props.ui.current_theme]}>
             <Carousel
@@ -126,9 +130,15 @@ function Menu(props) {
                 ref={c => setCarousel(c)}
                 onSnapToItem={onSnap}
             />
-            <View style={{position: 'absolute', top: 0, left: (props.ui.default_styles.window.width / 2)-45}}>
-                {/* <InStockIcon transform={[{ rotate: '-135deg' }]} fill={props.ui.current_theme.color} height={20} width={20} /> */}
-                <MenuSelectIcon fill={props.ui.current_theme.color} height={icon_size} width={icon_size} transform={[{scaleX:1.2}]} />
+            <View style={{position: 'absolute', marginTop: 10, borderColor: props.ui.border_color,borderWidth:1, width:100, height: 40, top: 10, left: (props.ui.default_styles.window.width / 2)-50}}>
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.top_right]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.top_left]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_right]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_left]} width={icon_size} height={icon_size} />
+                <View style={{alignSelf: 'center', marginTop: -10}}>
+                    <InStockIcon transform={[{ rotate: '45deg' }]} fill={props.ui.current_theme.color} height={20} width={20} />
+                </View>
+                {/* <MenuSelectIcon fill={props.ui.current_theme.color} height={icon_size} width={icon_size} transform={[{scaleX:1.2}]} /> */}
             </View>
         </View>
     )
@@ -155,6 +165,7 @@ function Menu(props) {
     // )
 }
 
+const icon_distance = -1
 const styles = StyleSheet.create({
     menu: {
         // flex: 1,
@@ -186,5 +197,13 @@ const styles = StyleSheet.create({
     selected: {
         borderBottomWidth: 1,
         borderStyle: 'solid'
-    }
+    },
+    corner_icon: {
+        zIndex: 10,
+        position: 'absolute'
+    },
+    top_right: { top: icon_distance, right: icon_distance },
+    top_left: { top: icon_distance, left: icon_distance, transform: [{ rotate: '-90deg' }] },
+    bottom_right: { bottom: icon_distance, right: icon_distance, transform: [{ rotate: '90deg' }] },
+    bottom_left: { bottom: icon_distance, left: icon_distance, transform: [{ rotate: '180deg' }] }
 })
