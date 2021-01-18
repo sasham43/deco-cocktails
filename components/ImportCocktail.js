08965d6bba34ddeecccc7ecb9490214d4f8ea9f7
Sasha Kramer <sashasemail@gmail.com>
2021-01-18 15:05:30 -0500

    style it up

--- components/ImportCocktail.js
+++ components/ImportCocktail.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { generate } from 'shortid'
 
 import AppText from './AppText'
 import AppButton from './AppButton'
 import {CompactView} from './CompactView'
+import CornerIcon from '../assets/corner'
 
 const mapStateToProps = (state) => {
     const { ui, stock } = state
@@ -21,17 +23,62 @@ export default connect(mapStateToProps, mapDispatchToProps)(ImportCocktail)
 
 // export default 
 function ImportCocktail(props){
+    const [cocktail, setCocktail] = useState(translateCocktail(props.cocktail))
+
+    var small_screen = Dimensions.get('window').height < 700
+    var modal_style = small_screen ? styles.small_share_modal : styles.large_share_modal
     function importCocktail(){
         console.log('import')
     }
-    function hideShareModal(){
-        props.hide()
+    function translateCocktail(imported){
+        const new_cocktail = {
+            ingredients: []
+        }
+        var index = -1 // so we can increment to 0
+        for(var prop in imported){
+            console.log('prop', prop)
+            if(prop.includes('ingredients')){
+                if(prop.includes('id')){
+                    index++
+                    new_cocktail.ingredients[index] = {
+                        id: generate()
+                    }
+                } else if (prop.includes('ingredient_name')){
+                    new_cocktail.ingredients[index].ingredient_name =  imported[prop]
+                    // new_cocktail.ingredients[index] = {
+                    //     ingredient_name: imported[prop]
+                    // }
+                } else if (prop.includes('parts')){
+                    new_cocktail.ingredients[index].parts = imported[prop]
+                }
+            } else {
+                new_cocktail[prop] = imported[prop]
+            }
+        }
+        // setCocktail(new_cocktail)
+        console.log('new cocktail', new_cocktail)
+        return new_cocktail
     }
+    // function hideShareModal(){
+    //     props.hide()
+    // }
+    const icon_size = 40
     return (
-        <View style={{padding: 50, justifyContent: 'center'}}>
-            <AppText>Import Cocktail</AppText>
-            <AppText>{props.cocktail.name}</AppText>
-            <CompactView ui={props.ui} cocktail={props.cocktail} stock={props.stock.current}  />
+        <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: props.ui.current_theme.backgroundColor, paddingTop: 30, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, flex: 1 }}>
+            {/* <AppText>Import Cocktail</AppText>
+            <AppText>{props.cocktail.name}</AppText> */}
+            <View style={[{ backgroundColor: props.ui.current_theme.backgroundColor, margin: 10, padding: 25, borderColor: props.ui.current_theme.color, borderWidth: 1 }, modal_style]}>
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.top_right]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.top_left]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_right]} width={icon_size} height={icon_size} />
+                <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_left]} width={icon_size} height={icon_size} />
+                <View>
+                    <AppText style={styles.cocktail_title}>{props.cocktail.name}</AppText>
+                </View>
+                <CompactView ui={props.ui} cocktail={cocktail} stock={props.stock.current}  />
+            </View>
+
+
             <View style={{ flexDirection: 'row' }}>
                 <View style={[{ marginRight: 5, flex: 1 }]} >
                     <AppButton press={importCocktail}>
@@ -39,7 +86,7 @@ function ImportCocktail(props){
                     </AppButton>
                 </View>
                 <View style={[{ marginLeft: 5, flex: 1 }]} >
-                    <AppButton style={[{ marginLeft: 100 }]} press={hideShareModal}>
+                    <AppButton style={[{ marginLeft: 100 }]} press={props.hide}>
                         Cancel
                     </AppButton>
                 </View>
@@ -47,3 +94,33 @@ function ImportCocktail(props){
         </View>
     )
 }
+const icon_distance = -1
+const styles = StyleSheet.create({
+    small_share_modal: {
+        // maxHeight: 575,
+        // maxWidth: 400,
+        // minHeight: 500,
+        // minWidth: 350
+        height: 550,
+        width: 350
+    },
+    large_share_modal: {
+        height: 600,
+        width: 350
+    },
+    corner_icon: {
+        zIndex: 10,
+        position: 'absolute'
+    },
+    top_right: { top: icon_distance, right: icon_distance },
+    top_left: { top: icon_distance, left: icon_distance, transform: [{ rotate: '-90deg' }] },
+    bottom_right: { bottom: icon_distance, right: icon_distance, transform: [{ rotate: '90deg' }] },
+    bottom_left: { bottom: icon_distance, left: icon_distance, transform: [{ rotate: '180deg' }] },
+
+    cocktail_title: {
+        // alignItems: 'center',
+        textAlign: 'center',
+        fontSize: 22,
+        // flex: 2,
+    },
+})
