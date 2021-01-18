d8746d0205de54507e91afba37c86727bf5cfa63
Sasha Kramer <sashasemail@gmail.com>
2021-01-18 14:47:07 -0500

    move compactview to separate component

--- components/ImportCocktail.js
+++ components/ImportCocktail.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import AppText from './AppText'
+import AppButton from './AppButton'
+import {CompactView} from './CompactView'
 
 const mapStateToProps = (state) => {
-    const { ui } = state
-    return { ui }
+    const { ui, stock } = state
+    return { ui, stock }
 }
 
 const mapDispatchToProps = dispatch => (
@@ -19,10 +21,29 @@ export default connect(mapStateToProps, mapDispatchToProps)(ImportCocktail)
 
 // export default 
 function ImportCocktail(props){
+    function importCocktail(){
+        console.log('import')
+    }
+    function hideShareModal(){
+        props.hide()
+    }
     return (
         <View style={{padding: 50, justifyContent: 'center'}}>
             <AppText>Import Cocktail</AppText>
             <AppText>{props.cocktail.name}</AppText>
+            <CompactView ui={props.ui} cocktail={props.cocktail} stock={props.stock.current}  />
+            <View style={{ flexDirection: 'row' }}>
+                <View style={[{ marginRight: 5, flex: 1 }]} >
+                    <AppButton press={importCocktail}>
+                        Import
+                    </AppButton>
+                </View>
+                <View style={[{ marginLeft: 5, flex: 1 }]} >
+                    <AppButton style={[{ marginLeft: 100 }]} press={hideShareModal}>
+                        Cancel
+                    </AppButton>
+                </View>
+            </View>
         </View>
     )
 }
