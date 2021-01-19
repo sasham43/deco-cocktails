a55109cee807ea1e75dfb6fbc10410eb780f21c9
Sasha Kramer <sashasemail@gmail.com>
2021-01-19 18:35:44 -0500

    import menu

--- components/ImportCocktail.js
+++ components/ImportCocktail.js
@@ -36,44 +36,6 @@ function ImportCocktail(props){
         props.addCocktail(cocktail)
         props.hide()
     }
-    // function translateCocktail(imported){
-    //     const new_cocktail = {
-    //         ingredients: []
-    //     }
-    //     // var index = -1 // so we can increment to 0
-    //     for(var prop in imported){
-    //         console.log('prop', prop)
-
-    //         if(prop == 'n'){
-    //             new_cocktail['name'] = imported[prop]
-    //         } else if (prop == 'd'){
-    //             new_cocktail['directions'] = imported[prop]
-    //         } else {
-    //             var num = parseInt(prop)
-    //             var type = prop.replace(`${num}`, '')
-
-    //             if(!new_cocktail.ingredients[num]){
-    //                 new_cocktail.ingredients[num] = {}
-    //             }
-
-    //             if(type == 'n'){
-    //                 new_cocktail.ingredients[num].ingredient_name = imported[prop]
-    //             } else if (type == 'p'){
-    //                 new_cocktail.ingredients[num].parts = imported[prop]
-    //             }
-    //         }
-    //     }
-    //     new_cocktail.ingredients = new_cocktail.ingredients.map(i=>{
-    //         return {...i, id: generate()}
-    //     })
-    //     new_cocktail.id = generate()
-    //     // setCocktail(new_cocktail)
-    //     console.log('new cocktail', new_cocktail)
-    //     return new_cocktail
-    // }
-    // function hideShareModal(){
-    //     props.hide()
-    // }
     const icon_size = 40
     return (
         <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: props.ui.current_theme.backgroundColor, paddingTop: 30, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, flex: 1 }}>
