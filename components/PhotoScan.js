d93b0e91fea4b3e7cd128985995ab92f933743c2
Sasha Kramer <sashasemail@gmail.com>
2021-01-20 19:21:01 -0500

    clean up

--- components/PhotoScan.js
+++ components/PhotoScan.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { View, Image, StyleSheet, Animated, Easing, SafeAreaView } from 'react-native'
+import { View, Image, StyleSheet, SafeAreaView } from 'react-native'
 import { BarCodeScanner } from 'expo-barcode-scanner'
-import * as Linking from 'expo-linking'
 import * as ImagePicker from 'expo-image-picker'
 
 
 import AppText from './AppText'
 import AppButton from './AppButton'
-import AppMenu from './AppMenu'
-import FunctionButtonIcon from '../assets/function-button'
 import CornerIcon from '../assets/corner'
 // import {translateForImport} from '../utils/translate'
 
@@ -53,15 +50,6 @@ export default function PhotoScan(props){
         return <AppText>No access to camera</AppText>;
     }
 
-    // function onSnap(carousel, index){
-    //     setCurrentIndex(index)
-    // }
-    // const items = [{
-    //     name: 'Scan'
-    // }, {
-    //     name: 'Import Photo'
-    // }]
-
     return (
         <SafeAreaView style={[{padding: 50, flex: 1}, props.ui.current_theme]}>
             <View style={{padding:40, flex: 1, justifyContent: 'space-between'}}>
@@ -95,28 +83,6 @@ export default function PhotoScan(props){
     )
 }
 
-// function ScanContent(props){
-//     if(props.index == 0){
-//         return (
-//             <View>
-//                 <BarCodeScanner
-//                     onBarCodeScanned={props.scanned ? undefined : props.handleBarCodeScanned}
-//                     style={{ height: 300 }}
-//                 />
-//                 {/* {scanned && <AppButton title={'Tap to Scan Again'} press={() => setScanned(false)}>Scan Again</AppButton>} */}
-//                 {/* <ScanText ui={props.ui} fill={props.ui.current_theme.color} scanned={props.scanned} setScanned={() => props.setScanned(false)} /> */}
-//             </View>
-//         )
-//     } else {
-//         return (
-//             <View >
-//                 {/* <AppText>Import</AppText> */}
-//                 <ImportImage ui={props.ui} handleBarCodeScanned={props.handleBarCodeScanned} fill={props.ui.current_theme.color} scanned={props.scanned} setScanned={() => props.setScanned(false)} />
-//             </View>
-//         )
-//     }
-// }
-
 function ImportImage(props){
     const [image, setImage] = useState(null);
 
@@ -125,10 +91,10 @@ function ImportImage(props){
             if (Platform.OS !== 'web') {
                 // says this function is undefined, not sure why it doesn't exist
                 //
-                // const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-                // if (status !== 'granted') {
-                //     alert('Sorry, we need camera roll permissions to make this work!');
-                // }
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
             }
         })();
     }, [])
@@ -142,23 +108,16 @@ function ImportImage(props){
             base64: true
         });
 
-        // console.log(result.base64);
-
         if (!result.cancelled) {
             setImage(result.uri);
-            // setImage(`data:image/jpeg;base64,${result.base64}`);
             console.log('image:', image)
             const qr = await BarCodeScanner.scanFromURLAsync(result.uri)
 
-            // console.log('qr result', qr, image)
             if(qr.length > 0){
-                // setQrStatus('found')
-                // await wait(400)
                 console.log('finished wait')
                 props.handleBarCodeScanned(qr[0])
             } else {
                 props.handleBarCodeScanned({})
-            //     // setQrStatus('not_found')
             }
         }
     };
@@ -171,14 +130,11 @@ function ImportImage(props){
                 <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_right_small]} width={15} height={15} />
                 <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.bottom_left_small]} width={15} height={15} />
 
-                {/* <AppText>Status:</AppText> */}
-                {/* <ScanAnimation ui={props.ui} fill={props.ui.current_theme.color} scanned={props.scanned} setScanned={() => props.setScanned(false)} /> */}
                 <View style={{margin:5, borderWidth:0}}>
                 <ScanMsg status={props.status} />
                 </View>
             </View>
             <AppButton style={{flex:1}} press={pickImage}>Select Photo</AppButton>
-            {/* <Image style={{ flex: 1, borderWidth: 1, width: 200, height: 200 }} source={{ uri: image}} resizeMode={'contain'} /> */}
         </View>
     )
 }
@@ -211,75 +167,6 @@ function ScanMsg(props){
     } else {
         return (
             <AppText>Scanning...</AppText>
-        )
-    }
-}
-
-function ScanAnimation(props){
-    if(props.scanned){
-        return (
-            <AppButton press={props.setScanned}>Scan Again</AppButton>
-        )
-    } else {
-        const [position, setPosition] = useState(new Animated.ValueXY(0, 0))
-        // const [position, setPosition] = useState(new Animated.ValueXY(0, 0))
-        // Animated.timing(position, {
-        //     toValue: { x: 200, y: 0 },
-        //     duration: 2000,
-        //     useNativeDriver: true
-        // }).start()
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(position, {
-                    toValue: { x: 180, y: 0 },
-                    duration: 8000,
-                    easing: Easing.linear,
-                    useNativeDriver: true
-                }),
-                Animated.timing(position, {
-                    toValue: { x: -50, y: 0 },
-                    duration: 8000,
-                    easing: Easing.linear,
-                    useNativeDriver: true
-                })
-            ])
-        ).start()
-
-        return (
-            <View style={{padding: 0, marginTop: 0, textAlign: 'center'}}>
-                {/* <AppText style={{textAlign: 'center', fontSize: 18}}>Scanning...</AppText> */}
-                <View style={{position: 'relative'}}>
-                    <Animated.View
-                        // style={position}
-                        style={
-                            {
-                                transform: position.getTranslateTransform()
-                            }
-                        }
-                        // transform={[{translateX: position.x}]}
-                    >
-                        <View style={{
-                            // backgroundColor: 'rgba(250,250,250,1)',
-                            backgroundColor: props.ui.current_theme.backgroundColor,
-                            // backgroundColor: 'rgba(0,50,50,1)',
-                            // backgroundColor: '#000000',
-                            // borderTopWidth:1,
-                            borderColor: '#000',
-                            marginTop: 10,
-                            height: 120,
-                            width: 120,
-                            // height: 35,
-                            // width: 35,
-                            position: 'absolute',
-                            zIndex: 20,
-                        }}
-                        ></View>
-                    </Animated.View>
-                    <View style={{zIndex:-11, flex: 1, alignItems: 'center'}}>
-                        <FunctionButtonIcon style={{zIndex:1}} fill={props.fill} width={100} height={100} />
-                    </View>
-                </View>
-            </View>
         )
     }
 }
