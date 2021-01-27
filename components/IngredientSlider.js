da86254c5b0fcc857948e771d98dd852ce525323
Sasha Kramer <sashasemail@gmail.com>
2021-01-27 18:12:11 -0500

    disable ingredientslider, just use for display

--- components/IngredientSlider.js
+++ components/IngredientSlider.js
@@ -25,39 +25,39 @@ function IngredientSlider(props){
     const [sliderValue, setSliderValue] = useState(0)
     const [ingredient, setIngredient] = useState(null)
     var slider_value = 0
-    const panResponder = useRef(PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
-        onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-        onMoveShouldSetPanResponder: (evt, gestureState) => true,
-        onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-        onPanResponderMove: (evt, gestureState) => {
-            var value = parseInt(gestureState.dx / 10)
-            var direction = gestureState.vx > 0 ? 'right' : 'left'
-            var slider_max = 1440
-            if((slider_value + value) > slider_max){
-                slider_value = slider_max
-            } else if ((slider_value - (value * -1)) < 0){
-                slider_value = 0
-            } else {
-                if(gestureState.vx > 0){
-                    slider_value = slider_value + value
-                } else {
-                    if(value.toString().includes("-")){
-                        slider_value += value
-                    } else {
-                        slider_value -= value
-                    }
-                }
-            }
+    // const panResponder = useRef(PanResponder.create({
+    //     onStartShouldSetPanResponder: (evt, gestureState) => true,
+    //     onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    //     onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    //     onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    //     onPanResponderMove: (evt, gestureState) => {
+    //         var value = parseInt(gestureState.dx / 10)
+    //         var direction = gestureState.vx > 0 ? 'right' : 'left'
+    //         var slider_max = 1440
+    //         if((slider_value + value) > slider_max){
+    //             slider_value = slider_max
+    //         } else if ((slider_value - (value * -1)) < 0){
+    //             slider_value = 0
+    //         } else {
+    //             if(gestureState.vx > 0){
+    //                 slider_value = slider_value + value
+    //             } else {
+    //                 if(value.toString().includes("-")){
+    //                     slider_value += value
+    //                 } else {
+    //                     slider_value -= value
+    //                 }
+    //             }
+    //         }
             
-            var constrained = constrain((slider_value / 30), 0, props.ingredient_values.length - 1)
-            setSliderValue(constrained)
+    //         var constrained = constrain((slider_value / 30), 0, props.ingredient_values.length - 1)
+    //         setSliderValue(constrained)
 
-            const ingredient = props.ingredient_values[constrained]
-            setIngredient(ingredient)
-            props.setParts(ingredient.value)
-        },
-    })).current
+    //         const ingredient = props.ingredient_values[constrained]
+    //         setIngredient(ingredient)
+    //         props.setParts(ingredient.value)
+    //     },
+    // })).current
 
     // if outside value changes, set inside value to match
     useEffect(()=>{
@@ -79,7 +79,7 @@ function IngredientSlider(props){
     return (
         <View>
             <View
-                {...panResponder.panHandlers}
+                // {...panResponder.panHandlers}
                 style={{ height: 30, borderColor: 'grey', borderWidth: 0, padding: 10, marginTop:-15 }}
             >
                 <CornerIcon fill={props.ui.current_theme.color} style={[styles.corner_icon, styles.top_right]} width={12} height={12} />
